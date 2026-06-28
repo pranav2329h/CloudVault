ea3783cbfc0c2214a46d4461efff67f65311d83f
pranav2329h <pranav2329h@gmail.com>
2026-06-28 15:41:03 +0530

    Initialize Terraform project

---
+++ backend/terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "project_name" {
+
+  value = var.project_name
+
+}
+
+output "region" {
+
+  value = var.aws_region
+
+}
---
+++ backend/terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+
+  description = "AWS Region"
+
+  type = string
+
+}
+
+variable "project_name" {
+
+  description = "Project Name"
+
+  type = string
+
+}
+
+variable "environment" {
+
+  description = "Environment"
+
+  type = string
+
+}
+
+variable "instance_type" {
+
+  description = "EC2 Instance Type"
+
+  type = string
+
+}
